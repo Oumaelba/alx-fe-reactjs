6692bc359c840f98326040c6e1a8ec72d1f47de4
Oumaelba <elbachirioumaima16@gmail.com>
2024-11-09 16:53:54 +0100

    1. Modify JSX in a Pre-built React Component

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -15,6 +15,7 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
+    
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
