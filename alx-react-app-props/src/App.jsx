311ef8b8887394af8f0d8aaecee586181ad2f3a6
Oumaelba <elbachirioumaima16@gmail.com>
2024-11-09 23:13:18 +0100

    2. Refactor Prop Drilling to Use Context API 4

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -6,7 +6,7 @@ import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
 import ProfilePage from './components/ProfilePage';
-import UserContext from './context/UserContext';
+import UserContext from './UserContext';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
--- alx-react-app-props/src/components/UserDetails.jsx
+++ alx-react-app-props/src/components/UserDetails.jsx
@@ -1,6 +1,6 @@
 // src/components/UserDetails.jsx
 import React, { useContext } from 'react';
-import UserContext from '../context/UserContext';
+import UserContext from '../UserContext';
 
 function UserDetails() {
   const userData = useContext(UserContext);
