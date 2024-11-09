f405e5702171f063c0232a9143449d310db4f7fc
Oumaelba <elbachirioumaima16@gmail.com>
2024-11-09 19:29:53 +0100

    0. Apply Inline CSS Styling to React Components

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+  return (
+    <main>
+      <p style={{backgroundColor: 'navy'}}>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
