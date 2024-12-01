8461efa8192a6663c380f65ec811fbc57453b2b4
Oumaelba <elbachirioumaima16@gmail.com>
2024-12-01 21:54:30 +0100

    4 0. Implementing Form Handling in React with Controlled Components and Formik

--- form-handling-react/src/components/FormikForm.js
+++ form-handling-react/src/components/FormikForm.js
@@ -1,24 +1,22 @@
-// src/components/FormikForm.js
 import React from 'react';
-import { Formik, Field, Form, ErrorMessage } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
-const FormikForm = () => {
+const formikForm = () => {
   const initialValues = {
     username: '',
     email: '',
     password: '',
   };
 
-  const validationSchema = Yup.object({
+  const validationSchema = Yup.object().shape({
     username: Yup.string().required('Username is required'),
     email: Yup.string().email('Invalid email format').required('Email is required'),
     password: Yup.string().required('Password is required'),
   });
 
   const handleSubmit = (values) => {
-    // Simulate form submission
-    console.log('Form submitted successfully:', values);
+    console.log('Form Submitted:', values);
   };
 
   return (
@@ -29,24 +27,24 @@ const FormikForm = () => {
     >
       <Form>
         <div>
-          <label>Username</label>
-          <Field type="text" name="username" />
-          <ErrorMessage name="username" component="p" />
+          <label htmlFor="username">Username</label>
+          <Field id="username" name="username" type="text" />
+          <ErrorMessage name="username" component="div" />
         </div>
         <div>
-          <label>Email</label>
-          <Field type="email" name="email" />
-          <ErrorMessage name="email" component="p" />
+          <label htmlFor="email">Email</label>
+          <Field id="email" name="email" type="email" />
+          <ErrorMessage name="email" component="div" />
         </div>
         <div>
-          <label>Password</label>
-          <Field type="password" name="password" />
-          <ErrorMessage name="password" component="p" />
+          <label htmlFor="password">Password</label>
+          <Field id="password" name="password" type="password" />
+          <ErrorMessage name="password" component="div" />
         </div>
-        <button type="submit">Register</button>
+        <button type="submit">Submit</button>
       </Form>
     </Formik>
   );
 };
 
-export default FormikForm;
+export default formikForm;
