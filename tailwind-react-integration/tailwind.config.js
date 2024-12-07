adc0dd21bc98d1c8ae411294fd6d36d8e99403e3
Oumaelba <elbachirioumaima16@gmail.com>
2024-12-07 20:55:44 +0100

    0. Setting Up Tailwind CSS in a New React Project

---
+++ tailwind-react-integration/tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"], // Paths to files to scan for classes
+  darkMode: false, // Set to 'media' or 'class' for dark mode
+  theme: { extend: {} }, // Customize your theme here
+  variants: { extend: {} },
+  plugins: [], // Add Tailwind plugins here if needed
+};
