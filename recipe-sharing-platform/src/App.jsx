d83903e35d7bc1dcee933f8b30e862ac1cd47be8
Oumaelba <elbachirioumaima16@gmail.com>
2024-12-07 21:24:23 +0100

    2. Building the Recipe Detail Page

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import react-router-dom
+import HomePage from './components/HomePage'; // Import HomePage component
+import RecipeDetail from './components/RecipeDetail';
 
 function App() {
   const [count, setCount] = useState(0)
